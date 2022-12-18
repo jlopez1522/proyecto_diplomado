685d1fe1e8e2acf1db152d7d1969e22226d64624
Jeisson Andres Lopez <jeissona.lopeza@gmail.com>
2022-12-18 17:27:41 -0500

    ajustes 2

--- view/lenguaje.php
+++ view/lenguaje.php
@@ -17,7 +17,7 @@
     <header>
         <!-- Navbar -->
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="inicio.html">IngSoft</a>
+            <a class="navbar-brand" href="inicio.php">IngSoft</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
                 aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
